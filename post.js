3ef05ac9f2019c98243bb69c939c7fd5e143270b
Rishabh Pandey <rishabhp48@gmail.com>
2021-06-22 15:47:58 +0530

    Add files via upload

--- post.js
+++ post.js
@@ -1,8 +1,42 @@
-const express = require("express");
-const { create, view } = require("../controllers/post");
-const router = express.Router();
+const chalk = require("chalk");
+const post = require("../models/post");
 
-router.post("/post", create);
-router.get("/posts", view);
+exports.create = (req, res) => {
+  const { name, phoneno, email, industry, about } = req.body;
 
-module.exports = router;
+  switch (true) {
+    case !name:
+      return res.status(400).json({ message: "Name is required" });
+      break;
+    case !phoneno:
+      return res.status(400).json({ message: "phoneno is required" });
+      break;
+    case !email:
+      return alert("Email is not valid");
+      break;
+    case !industry:
+      return res.status(400).json({ message: "industry is required" });
+      break;
+    case !about:
+      return res.status(400).json({ message: "about is required" });
+      break;
+  }
+  post.create({ name, phoneno, email, industry, about }, (err, data) => {
+    if (!err) {
+      res.json(data);
+      console.log(chalk.bgGreenBright.black(data));
+    } else {
+      console.log(chalk.bgRedBright.black(err));
+    }
+  });
+};
+
+exports.view = (req, res) => {
+  post.find({}, (err, data) => {
+    if (!err) {
+      res.json(data);
+    } else {
+      res.json(err);
+    }
+  });
+};
